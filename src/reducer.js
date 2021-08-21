10c782b8a550fd878bfbf6d855028063c6691ac6
dangvannhang <dangvannhang107@gmail.com>
2021-08-22 00:15:31 +0700

    Nothing

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,10 @@
+import { CLEAR_CART } from './constants/actions'
 // reducer - regular function js -  method to handle store off app
 function reducer(state, action) {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [], total: 0, amount: 0 }
+  }
+
   console.log('otherr value')
   return state
 }
