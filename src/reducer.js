d30d014e231dc9ec73c3989654f5d00b70169399
dangvannhang <dangvannhang107@gmail.com>
2021-08-22 16:14:18 +0700

    Calculate amount and total

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,10 @@
-import { CLEAR_CART, INCREASE, DECREASE, REMOVE } from './constants/actions'
+import {
+  CLEAR_CART,
+  INCREASE,
+  DECREASE,
+  REMOVE,
+  GET_TOTALS,
+} from './constants/actions'
 
 // reducer - regular function js -  method to handle store off app
 function reducer(state, action) {
@@ -18,22 +24,15 @@ function reducer(state, action) {
     }
     case DECREASE: {
       console.log('you decreased')
-      let tempCart = []
-      if (action.payload.amount === 1) {
-        console.log(`hey it is one`)
-        tempCart = state.cart.filter(
-          (cartItem) => cartItem.id !== action.payload.id
-        )
-      } else {
-        tempCart = state.cart.map((cartItem) => {
-          if (cartItem.id === action.payload.id) {
-            return { ...cartItem, amount: cartItem.amount - 1 }
-          }
-          if (cartItem.amount <= 0) {
-          }
-          return cartItem
-        })
-      }
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          return { ...cartItem, amount: cartItem.amount - 1 }
+        }
+        if (cartItem.amount <= 0) {
+        }
+        return cartItem
+      })
+
       return {
         ...state,
         cart: tempCart,
@@ -47,6 +46,27 @@ function reducer(state, action) {
           (cartItem) => cartItem.id !== action.payload.id
         ),
       }
+    }
+    case GET_TOTALS: {
+      console.log('get total')
+      let { total, amount } = state.cart.reduce(
+        // gia tri thang du, gia tri moi vong lap
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem
+          const itemTotal = price * amount
+          cartTotal.total += itemTotal
+
+          cartTotal.amount += amount
+          return cartTotal
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      )
+      // only get 2 digit after ,
+      total = parseFloat(total.toFixed(2))
+      return { ...state, total, amount }
     }
     default:
       return state
